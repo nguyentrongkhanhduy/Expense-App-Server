be5ffa2b2e203a175d7a6a84fa3fefcfd5001a7d
davidHuyDang <davidhuydang02@gmail.com>
2025-06-17 19:09:10 -0400

    TEDJ-47 Host server using available tools

--- Server/routes/authentication.js
+++ Server/routes/authentication.js
@@ -2,6 +2,7 @@ const express = require("express");
 const router = express.Router();
 const { signIn, signUp } = require("../controllers/authenticationController");
 
+
 router.post("/signup", signUp);
 router.post("/signin", signIn);
 
