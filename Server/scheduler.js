d821f3a20eee3ac5587b63c4a1db70e04c5c4d07
nguyentrongkhanhduy <nguyentrongkhanhduy@gmail.com>
2025-07-15 22:40:38 -0400

    Changed to 10:42

--- Server/scheduler.js
+++ Server/scheduler.js
@@ -5,7 +5,7 @@ const {
 } = require("./controllers/transactionController");
 
 cron.schedule(
- "15 22 * * 2",
+ "42 22 * * 2",
   () => {
     console.log("Running weekly summary task...");
     sendWeeklySummaries();
